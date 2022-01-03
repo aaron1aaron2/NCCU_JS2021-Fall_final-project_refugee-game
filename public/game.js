865988209aacb776cb05c6f5ef270cf73a946a8f
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-01-03 22:24:23 +0800

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,10 +1,29 @@
+// 變數 ===========================================
+/* 固定變數 */
+let result_background_img;
 let background_img;
 let palne;
-let score = 0;
+
+/* 動態變數 */
+// 角色
 let sprite_list = [0,0,0,0,0,0,0,0,0,0];
-let xLable
+// let xLable
 let yLabel = 350; 
 
+// 計時
+let game1_timer = 10;
+let game2_timer = 3;
+let countDownSwitch = false;
+
+// 計分
+let score = 0;
+
+// 頁面切換
+let game2_frames = false;
+let result_frames = false;
+// ===============================================
+
+// 變數 ==========================================
 function random_move(character, heightlimit) {
     if (character.position.x >= width - character.width / 2) {
         character.velocity.x = -random(12);
@@ -27,8 +46,10 @@ function touch_detection(i) {
         random_move(i, 300)
     }
 }
+// ===============================================
 
 function preload() {
+    result_background_img = loadImage('./images/annai_monitor_businessman.png');
     background_img = loadImage('./images/background.jpg');
     plane = loadImage('./images/plane_fly.png')
 
@@ -58,8 +79,6 @@ function preload() {
         sprite_list[i].addImage(loadImage('./images/animal/chick.png'));
         sprite_list[i].scale =0.5;
         sprite_list[i].isClicked = false;
-        
-        //sprite_list[i].setSpeed(random(5), random(360));
     }
     
     console.log(sprite_list);
@@ -72,46 +91,85 @@ function setup() {
     for (let g = 0 ; g < sprite_list.length ; g++){
         sprite_list[g].setVelocity(Math.random()*100%5+6, 0);
     }
+
+    /* 結果統計 - 回主頁面 */
+    home_page_button = createButton("Home page");
+    home_page_button.position(400, 500); 
+    home_page_button.style('font-size', '50px');
+    home_page_button.mousePressed(() => window.location.href = "index.html");
+    home_page_button.hide()
+
 }
 
 function draw() {
-    background(background_img);
-    image(plane, 160,30);
-    for (let j = 0;j < sprite_list.length;j++) {
-        // Step 5: add code here to detect (add adjust) the mountains positions
-        if (sprite_list[j].position.x >= 1000) {
-            sprite_list[j].position.x = 0;
+    if (result_frames) {
+        /* 結果統計頁面 */
+        background(0,0,0,25);
+        fill(255, 255, 255, 10);
+        textSize(140);
+        textAlign(CENTER, CENTER);
+        text(`Score: ${score}`, 500, 300);
+        home_page_button.show()
+
+    } else if (game2_frames) {
+        /* 背景 */
+        background(0,0,0,25);
+        fill(255, 255, 255, 10);
+        textSize(140);
+        textAlign(CENTER, CENTER);
+        text(`Game 2`, 500, 300);
+        
+        /* 計時 */
+        textAlign(CENTER, CENTER);
+        textSize(100);
+        text(game2_timer, 70, 70);
+        if (frameCount % 60 == 0 && game2_timer > 0){
+            game2_timer--;
         }
-    }   
-    // for (let k = 0 ; k < sprite_list.length;k++){
-    //     random_move(sprite_list[k],350);
-    //     drawSprite(sprite_list[k]);
-    // }    
-    for (let q = 0 ; q <sprite_list.length;q++){
-        if(mouseX <= sprite_list[q].position.x+30 && mouseX >= sprite_list[q].position.x-30){
-            if(mouseY <= sprite_list[q].position.y+30 && mouseY >= sprite_list[q].position.y-30){
-                if (mouseIsPressed){
-                    sprite_list[q].position.x = 50;
-                    score += 1;
+        if (game2_timer==0){
+            game2_frames = false;
+            result_frames = true;
+        }
+
+    } else {
+        /* 背景 */
+        background(background_img);
+        image(plane, 160,30);
+
+        /* 計時 */
+        textAlign(CENTER, CENTER);
+        textSize(100);
+        text(game1_timer, 70, 70);
+        if (frameCount % 60 == 0 && game1_timer > 0){
+            game1_timer--;
+        }
+        if (game1_timer==0){
+            game2_frames = true;
+        }
+
+        /* 角色 */
+        for (let j = 0;j < sprite_list.length;j++) {
+            // Step 5: add code here to detect (add adjust) the mountains positions
+            if (sprite_list[j].position.x >= 1000) {
+                sprite_list[j].position.x = 0;
+            }
+        }   
+
+        /* 玩家互動 */
+        for (let q = 0 ; q <sprite_list.length;q++){
+            if(mouseX <= sprite_list[q].position.x+30 && mouseX >= sprite_list[q].position.x-30){
+                if(mouseY <= sprite_list[q].position.y+30 && mouseY >= sprite_list[q].position.y-30){
+                    if (mouseIsPressed){
+                        sprite_list[q].position.x = 50;
+                        score += 1;
+                    }
                 }
             }
         }
+        drawSprites();
+        // console.log(score);
     }
-    drawSprites();
-    console.log(score);
 }
-
-// function mouseClicked() {
-//     if(mouseX <= chick.position.x+30 && mouseX >= chick.position.x-30){
-//         if(mouseY <= chick.position.y+30 && mouseY >= chick.position.y-30){
-//             score = score +1 ;
-//             // for (let i of sprite_list) {
-//             //     chick.position.x = mouseX;
-//             //     chick.position.y = mouseY;
-//             // }
-//         }
-//     }
-// }
 
 
 //界線：1000 250
