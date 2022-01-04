a9eaf9955d4434e07dc871c9f8467d3f365db186
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-01-04 16:15:36 +0800

    add new role soldier

--- public/game.js
+++ public/game.js
@@ -1,11 +1,11 @@
 // 變數 ===========================================
-/* 固定變數 */
+/* 路徑變數 */
 // 遊戲中
 let game_background_img_path = './images/background.jpg';
 let game_palne_path = './images/UI/game/plane.png';
 let game_palne_colse_path = './images/UI/game/plane_close.png'; // 尚未使用
 let game_people_path = './images/role/people.png'
-
+let game_bonus_soldier_path  = './images/role/soldier.png';
 let game_stop_button_path = './images/UI/game/stop.png' // 尚未使用 // 進入暫停
 let game_timebox_path = './images/UI/game/time_box.png' // 時間條得框  // 尚未使用
 let game_timebox_now_path = './images/UI/game/time_now.png' // 時間條得內容物 // 尚未使用
@@ -23,15 +23,21 @@ let result_success_frame_path = './images/UI/result/success.png' //成功頁面
 let result_failed_frame_path = './images/UI/result/failed.png' //成功頁面
 
 
-/* 動態變數 */
+/* 遊戲變數 */
 // 角色
 let sprite_list = [0,0,0,0,0,0,0,0,0,0];
+let sprite_list_bonus = [0]; //要加新角色就多用一個list
+let sprite_list_all = [...sprite_list,...sprite_list_bonus];
 // let xLable
 let yLabel = 350; 
+let yLabelList = [350,400,450,500,550];
+let people_run_speed_base = 6; // 公式: Math.random()*100%5 + base speed
+let soldier_run_speed_base = 18;
 
 // 計時
 let prepare_timer = 4; // 1秒視顯示 start
 let game1_timer = 10;
+let game1_special_role_show_timer = 5;
 let game2_timer = 3;
 let countDownSwitch = false;
 
@@ -68,53 +74,51 @@ function touch_detection(i) {
         random_move(i, 300)
     }
 }
-// ===============================================
 
+function getRandomInt(num) {
+    return Math.floor(Math.random() * num);  // %5 代表num = 5 ->只會輸出0 1 2 3 4
+}
 
 // p5js ==========================================
 function preload() {
     background_img = loadImage(game_background_img_path);
-    plane = loadImage(game_palne_path)
+    plane = loadImage(game_palne_path);
 
-    for(let i = 0 ; i < sprite_list.length ; i++){
-        let way = i % 6;
-        switch(way){
-            case 0 :
-                yLabel = 350 ;
-                break;
-            case 1 :
-                yLabel = 400 ;
-                break
-            case 2 :
-                yLabel = 450 ;
-                break    
-            case 3 :
-                yLabel = 500 ;
-                break    
-            case 4 :
-                yLabel = 500 ;
-                break 
-            case 5 :
-                yLabel = 550 ;
-                break 
-            }
-        sprite_list[i] = createSprite(Math.random()*1000%1000+50 ,yLabel);
-        sprite_list[i].addImage(loadImage(game_people_path));
-        sprite_list[i].scale =0.5;
-        sprite_list[i].isClicked = false;
+    for(let i = 0 ; i < sprite_list_all.length;i++){
+        let ran = getRandomInt(5);
+
+        /*如果要加角色要在這裡加入新的*/
+        if (i < sprite_list.length){
+            sprite_list_all[i] = createSprite(Math.random()*1000%1000+50, yLabelList[ran]);
+            sprite_list_all[i].addImage(loadImage(game_people_path));
+            sprite_list_all[i].role = "People";   
+            sprite_list_all[i].scale =0.3;
+            sprite_list_all[i].isClicked = false;            
+        }
+        else if (i >= sprite_list.length && i < sprite_list_all.length){
+            sprite_list_all[i] = createSprite(Math.random()*1000%1000+50, yLabelList[ran]);
+            sprite_list_all[i].addImage(loadImage(game_bonus_soldier_path));
+            sprite_list_all[i].role = "Soldier";
+            sprite_list_all[i].scale = 0.3;
+            sprite_list_all[i].isClicked = false;
+        }
     }
-    
-    console.log(sprite_list);
 }
 
 function setup() {
     createCanvas(1000, 600);
     plane.width = 700;
-    for (let g = 0 ; g < sprite_list.length ; g++){
-        sprite_list[g].setVelocity(Math.random()*100%5+6, 0);
-    }
 
-    /* 結果統計 - 回主頁面 */
+    /* 角色移動 */ 
+    for (let g = 0 ; g < sprite_list_all.length ; g++){
+        if (sprite_list_all[g].role == "People"){
+            sprite_list_all[g].setVelocity(Math.random()*100%5+people_run_speed_base, 0);
+        }
+        else if (sprite_list_all[g].role == "Soldier"){
+            sprite_list_all[g].setVelocity(Math.random()*100%5+soldier_run_speed_base, 0);
+        }
+    }
+    /* 結果統計  */
     home_page_button = createButton("Home page");
     home_page_button.position(230, 500); 
     home_page_button.style('font-size', '50px');
@@ -147,6 +151,7 @@ function draw() {
         if (prepare_timer==1){
             text('Start !', 500, 300);
         }
+        
         /* 畫面切換 */
         if (prepare_timer==0){
             prepare_frames = false;
@@ -164,6 +169,7 @@ function draw() {
         text(game1_timer, 70, 70);
         if (frameCount % 60 == 0 && game1_timer > 0){
             game1_timer--;
+            game1_special_role_show_timer--;
         }
 
         /* 畫面切換 */
@@ -173,25 +179,47 @@ function draw() {
         }
 
         /* 角色 */
-        for (let j = 0;j < sprite_list.length;j++) {
+        for (let j = 0;j < sprite_list_all.length;j++) {
             // S超出界線則重製
-            if (sprite_list[j].position.x >= 1000) {
-                sprite_list[j].position.x = 0;
+            if (sprite_list_all[j].position.x >= 1000) {
+                sprite_list_all[j].position.x = 0;
+                // 跑完一段後隨機改變跑道 
+                let new_ran = getRandomInt(5);
+                sprite_list_all[j].position.y = yLabelList[new_ran];
+                // 特殊角色要隨機改變
+                // if(sprite_list_all[j].role == "Soldier"){
+                //     let new_ran = getRandomInt(5);
+                //     sprite_list_all[j].position.y = yLabelList[new_ran];
+                // }
             }
-        }   
+            // 控制角色出現時間
+            if (sprite_list_all[j].role == "People") {
+                drawSprite(sprite_list_all[j]);
+            } else {
+                if (game1_special_role_show_timer <= 0){
+                    drawSprite(sprite_list_all[j]);
+                }
+            }
+        }
 
         /* 玩家互動 */
-        for (let q = 0 ; q <sprite_list.length;q++){
-            if(mouseX <= sprite_list[q].position.x+30 && mouseX >= sprite_list[q].position.x-30){
-                if(mouseY <= sprite_list[q].position.y+30 && mouseY >= sprite_list[q].position.y-30){
+        for (let q = 0 ; q <sprite_list_all.length;q++){
+            if(mouseX <= sprite_list_all[q].position.x+30 && mouseX >= sprite_list_all[q].position.x-30){
+                if(mouseY <= sprite_list_all[q].position.y+30 && mouseY >= sprite_list_all[q].position.y-30){
                     if (mouseIsPressed){
-                        sprite_list[q].position.x = 50;
-                        score += 1;
+                        sprite_list_all[q].position.x = 50;
+                        if (sprite_list_all[q].role == "People"){
+                            score += 1;
+                        }
+                        else if (sprite_list_all[q].role == "Soldier"){
+                            score += 2;
+                        }
                     }
                 }
             }
         }
-        drawSprites();
+
+        // drawSprites();
 
     } else if (game2_frames) {
         /* 背景 */
@@ -228,8 +256,6 @@ function draw() {
 }
 // ===============================================
 
-
-//界線：1000 250
 
 // function mouseDragged() {
 //     chick.position.x = mouseX;
