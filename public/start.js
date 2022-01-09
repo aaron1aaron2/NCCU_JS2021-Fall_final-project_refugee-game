0754cfa3e1f1da7eb11fc768563c55ee96105da4
何彥南 (yen-nan ho) <46644821+aaron1aaron2@users.noreply.github.com>
2022-01-09 15:47:34 +0800

    debug canvas position

--- public/start.js
+++ public/start.js
@@ -16,7 +16,6 @@ let game_started = false;
 // p5js ==========================================
 function preload() {
     background_img = loadImage(background_img_path);
-
     clouds[0]=createSprite(900, 190);
     clouds[0].addImage(loadImage(background_clouds_img_path));
     
@@ -27,30 +26,36 @@ function preload() {
     clouds[2]=createSprite(50, 50);
     clouds[2].addImage(loadImage(background_cloud_img_path));
 
+   
+    // clouds[3]=createSprite(50, 100);
+    // clouds[3].addImage(loadImage(background_cloud_img_path));
 }
 
 function setup() {
     createCanvas(1000, 600);
     background(background_img);
-
     game_title = createImg(game_title_path);
-    game_title.position(200, -70); 
+    game_title.position(265, -20); 
 
     // background cloud的 移動
     clouds[0].setVelocity(-0.4, 0);
     clouds[1].setVelocity(-0.3, 0.02);
     clouds[2].setVelocity(-0.2, 0);
-
-
+    // clouds[3].setVelocity(-0.3, 0);
     // button:遊戲介紹
+    // intro_button = createButton("遊戲介紹");
+    // intro_button.position(400, 350); 
     intro_button = createImg(intro_game_button_path);
-    intro_button.position(350, 350); 
+    intro_button.position(415, 430); 
     intro_button.style('font-size', '50px');
     intro_button.mousePressed(() => intro_started = true);
 
     // button:遊戲開始
+    // start_button = createButton("開始遊戲");
+    // start_button.position(400, 430); 
+    
     start_button = createImg(start_game_button_path);
-    start_button.position(350, 430); 
+    start_button.position(415, 510); 
     start_button.style('font-size', '50px');
     start_button.mousePressed(() => game_started = true);
 }
@@ -69,7 +74,15 @@ function draw() {
         clouds[2].position.x = 1000;
     }
 
+    // if (clouds[3].position.x <= -30) {
+    //     clouds[3].position.x = 1000;
+    // }
+
+    // 標題
     drawSprites();
+    // textSize(100);
+    // textAlign(CENTER);
+
 
     if (game_started) {
         window.location.href = "game.html" ;
